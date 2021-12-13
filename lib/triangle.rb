29768190d1b2c868ecb7362cdfc4c33a4704dd69
Dan Seminara <japaneseforgaming@gmail.com>
2021-12-13 13:36:43 -0500

    commit tree 884f70a012d89f52114948f5f887f73a3b7f5fa2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  
+
+  attr_reader :kind
+
+  def initialize(a, b, c)
+
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+
+      elsif (a+b)<=c || (a+c)<=b || (b+c)<=a
+        raise TriangleError
+    
+      elsif a == b && a == c
+        @kind = :equilateral
+
+      elsif a == b || a == c || b == c
+        @kind = :isosceles
+
+      else
+        @kind = :scalene
+    end
+
+  end
+
+  
+
+  class TriangleError < StandardError
+    def message
+      "lkh"
+    end
+  end
+
+
+
+
 end
+
+
